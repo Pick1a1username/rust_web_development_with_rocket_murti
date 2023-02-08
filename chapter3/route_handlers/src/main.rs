892b919d3d786587630d970e014e610add40ddf1
Pick1a1username <xscreensaver@outlook.com>
2023-02-08 22:25:10 +0900

    20230208_0

--- chapter3/route_handlers/src/main.rs
+++ chapter3/route_handlers/src/main.rs
@@ -1,14 +1,28 @@
+// Todo: Chapter 3 Built-in implementations
+
 #[macro_use]
 extern crate rocket;
 
 use std::collections::HashMap;
-use rocket::{FromParam, Build, Rocket};
+use std::io::Cursor;
+
+use rocket::{Build, Rocket};
+use rocket::response::{self, Responder, Response};
+use rocket::request::FromParam;
+use rocket::http::ContentType;
 use lazy_static::lazy_static;
 
 #[derive(FromForm)]
 struct Filters {
     age: u8,
     active: bool,
+}
+
+fn default_response<'r>() -> response::Response<'r> {
+    Response::build()
+        .header(ContentType::Plain)
+        .raw_header("X-CUSTOM-ID", "CUSTOM")
+        .finalize()
 }
 
 #[derive(Debug)]
@@ -20,14 +34,57 @@ struct User {
     active: bool,
 }
 
+impl<'r> Responder<'r, 'r> for &'r User {
+    fn respond_to(self, _: &'r rocket::Request<'_>) -> response::Result<'r> {
+        let base_response = default_response();
+        let user = format!("Found user: {:?}", self);
+        Response::build()
+            .sized_body(user.len(), Cursor::new(user))
+            .raw_header("X-USER-ID", self.uuid.to_string())
+            .merge(base_response)
+            .ok()
+    }
+}
+
 struct NameGrade<'r> {
     name: &'r str,
-    grades: u8,
+    grade: u8,
 }
 
 impl<'r> FromParam<'r> for NameGrade<'r> {
     type Error = &'static str;
-    fn from_param(param: &'r str) -> Result<Self, Self::Error> {} // Todo
+    fn from_param(param: &'r str) -> Result<Self, Self::Error> {
+        const ERROR_MESSAGE: Result<NameGrade, &'static str> = Err("Error parsing user parameter");
+        let name_grade_vec: Vec<&'r str> = param.split('_').collect();
+        match name_grade_vec.len() {
+            2 => match name_grade_vec[1].parse::<u8> (){
+                Ok(n) => Ok(Self {
+                    name: name_grade_vec[0],
+                    grade: n,
+                }),
+                Err(_) => ERROR_MESSAGE,
+            },
+            _ => ERROR_MESSAGE,
+        }
+    }
+}
+
+struct NewUser<'a>(Vec<&'a User>);
+
+impl<'r> Responder<'r, 'r> for NewUser<'r> {
+    fn respond_to(self, _: &'r rocket::Request<'_>) -> response::Result<'r> {
+        let base_response = default_response();
+        let user =self
+            .0
+            .iter()
+            .map(|u| format!("{:?}", u))
+            .collect::<Vec<String>>()
+            .join(",");
+        Response::build()
+            .sized_body(user.len(), Cursor::new(user))
+            .join(base_response)
+            .ok()
+    }
 }
 
 lazy_static! {
@@ -37,7 +94,7 @@ lazy_static! {
             "3e3dd4ae-3c37-40c6-aa64-7061f284ce28",
             User {
                 uuid: "3e3dd4ae-3c37-40c6-aa64-7061f284ce28".to_string(),
-                name: "John Doe".to_string(),
+                name: "John".to_string(),
                 age: 8,
                 grade: 99,
                 active: true,
@@ -48,16 +105,33 @@ lazy_static! {
 }
 
 #[route(GET, uri = "/user/<uuid>", rank = 1, format = "text/plain")]
-fn user(uuid: &str) -> String { 
+fn user(uuid: &str) -> Option<&User> { 
     let user = USERS.get(uuid);
     match user {
-        Some(u) => format!("Found user: {:?}", u),
-        None => String::from("User not found"),
+        Some(u) => Some(u),
+        None => None,
     }
 }
 
-#[route(GET, uri = "/user/<name_grade>?<filters..>")]
-fn users(name_grade: u8, filters: Filters) { /* ... */}
+#[route(GET, uri = "/users/<name_grade>?<filters..>")]
+fn users(name_grade: NameGrade, filters: Option<Filters>) -> Option<NewUser> {
+    let users: Vec<&User> = USERS
+        .values()
+        .filter(|user| user.name.contains(&name_grade.name) && user.grade == name_grade.grade)
+        .filter(|user| {
+            if let Some(fts) = &filters {
+                user.age == fts.age && user.active == fts.active
+            } else {
+                true
+            }
+        })
+        .collect();
+    if users.len() > 0 {
+        Some(NewUser(users))
+    } else {
+        None
+    }
+}
 
 #[launch]
 fn rocket() -> Rocket<Build> {
