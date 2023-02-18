965388ececd3df995b9bda4a0df936f1a9a5885f
Pick1a1username <xscreensaver@outlook.com>
2023-02-18 21:48:02 +0900

    20230218_1

--- chapter5/our_application/src/main.rs
+++ chapter5/our_application/src/main.rs
@@ -1,3 +1,5 @@
+// Todo: Chapter5 Making user-generated contents
+
 #[macro_use]
 extern crate rocket;
 
@@ -10,6 +12,7 @@ use rocket_db_pools::{
     Connection,
     Database
 };
+use rocket::form::{self, DataField, Form, FromFormField, ValueField};
 use uuid::Uuid;
 
 #[derive(sqlx::Type, Debug)]
@@ -19,7 +22,7 @@ enum UserStatus {
     Active = 1,
 }
 
-#[derive(Debug, FromRow)]
+#[derive(Debug, FromRow, FromForm)]
 struct User {
     uuid: Uuid,
     username: String,
@@ -27,8 +30,8 @@ struct User {
     password_hash: String,
     description: String,
     status: UserStatus,
-    created_at: DateTime<Utc>,
-    updated_at: DateTime<Utc>,
+    created_at: OurDateTime,
+    updated_at: OurDateTime,
 }
 
 #[derive(Database)]
@@ -37,8 +40,62 @@ struct DBConnection(PgPool);
 
 type HtmlResponse = Result<RawHtml<String>, Status>;
 
+#[derive(Debug, FromRow, FromFormField)]
+struct OurDateTime(DateTime<Utc>);
+
+#[rocket::async_trait]
+impl<'r> FromFormField<'r> for OurDateTime {
+    fn from_value(_: ValueField<'r>) -> form::Result<'r, Self> {
+        todo!("will implement later")
+    }
+    async fn from_data(_: DataField<'r, '_>) -> form::Result<'r, Self> {
+        todo!("will implement later")
+    }
+}
+
+#[derive(FromForm)]
+struct Pagination {
+    cursor: OurDateTime,
+    limit: usize,
+}
+
 #[get("/users/<_uuid>", format = "text/html")]
 async fn get_user(mut _db: Connection<DBConnection>, _uuid: &str) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[get("/users?<_pagination>", format = "text/html")]
+async fn get_users(mut _db: Connection<DBConnection>, _pagination: Option<Pagination>) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[get("/users/new", format = "text/html")]
+async fn new_user(mut _db: Connection<DBConnection>) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[post("/users", format = "text/html", data = "<_user>")]
+async fn create_user(mut _db: Connection<DBConnection>, _user: Form<User>) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[get("/users/edit/<_uuid>", format = "text/html")]
+async fn edit_user(mut _db: Connection<DBConnection>, _uuid: &str) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[put("/users/<_uuid>", format = "text/html", data = "<_user>")]
+async fn put_user(mut _db: Connection<DBConnection>, _uuid: &str, _user: Form<User>) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[patch("/users/<_uuid>", format = "text/html", data = "<_user>")]
+async fn patch_user(mut _db: Connection<DBConnection>, _uuid: &str, _user: Form<User>) -> HtmlResponse {
+    todo!("will implement later")
+}
+
+#[delete("/users/<_uuid>", format = "text/html")]
+async fn delete_user(mut _db: Connection<DBConnection>, _uuid: &str) -> HtmlResponse {
     todo!("will implement later")
 }
 
